934e8ad08a96d310cfc8caf7158aca010bd18a88
Rodrigo Crespo <rcrespocepeda@gmail.com>
2017-04-28 12:12:41 +0200

    floor

--- js/floor.js
+++ js/floor.js
@@ -1,14 +1,13 @@
 var Floor = function(textureManager){
 
     var texture = textureManager.getTexture("skyline");
-    var ratio = texture.height / texture.width;
-    var cityWidth = SCREEN_WIDTH / 2.5;
-    var cityHeight = SCREEN_WIDTH / 2.5;
+    var ratio = texture.image.height / texture.image.width;
+    var cityWidth = SCREEN_WIDTH;
+    var cityHeight = SCREEN_WIDTH * ratio;
     this.geometry = new THREE.PlaneGeometry( cityWidth , cityHeight);
-    this.material = new THREE.MeshBasicMaterial( {color: 0xFF0000, side: THREE.DoubleSide, transparent: true} );
-    this.plane = new THREE.Mesh( this.eometry, this.material );
-    this.plane.position.set(0, cityHeight /2, -5);
-    this.plane.rotation.set(Math.PI/2, 1, 1);
+    this.material = new THREE.MeshBasicMaterial( {color: 0xf4a460, side: THREE.DoubleSide, transparent: true} );
+    this.plane = new THREE.Mesh( this.geometry, this.material );
+    this.plane.position.set(0, -50, -5);
+    this.plane.rotation.set(Math.PI/2, 0, 0);
 
 }
-
--- js/player.js
+++ js/player.js
@@ -5,7 +5,7 @@ var Player = function(textureManager){
 Player.prototype.init = function(){
 
     this.animatedTexture = new TextureAnimator( this.texture, 10, 1, 10, 75 ); // texture, #horiz, #vert, #total, duration.
-    var runnerMaterial = new THREE.MeshBasicMaterial( { map: this.texture, side: THREE.DoubleSide } );
+    var runnerMaterial = new THREE.MeshBasicMaterial( { map: this.texture, side: THREE.DoubleSide, transparent: true } );
     var runnerGeometry = new THREE.PlaneGeometry(50, 50, 1, 1);
     this.runner = new THREE.Mesh(runnerGeometry, runnerMaterial);
     this.runner.position.set(0, 0, 0);
