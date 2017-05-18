d2f3b769058bb3e5cd007eb523848ca448c431f3
Daniel-BG <danielbasgar@gmail.com>
2017-05-18 12:11:19 +0200

    more camera effects and added the possibility of stopping camera updates

--- js/helpers/camera_effects.js
+++ js/helpers/camera_effects.js
@@ -5,8 +5,10 @@ const AXIS_Z = "z";
 var CameraEffects = function() {
 	this.effects = {
 		zoomroll: effect_animation2Start(10, 2),
-		lookahead: effect_lookaxis(60, 2000, 100, AXIS_Y),
-		lookup: effect_lookaxis(-30, 2000, 100, AXIS_X)
+		lookaheadandback: effect_lookaxis(60, 2000, 100, AXIS_Y),
+		lookup: effect_lookaxis(-30, 2000, 100, AXIS_X),
+		lookahead: effect_lookaxis_nocomeback(30, 1000, 100, AXIS_Y),
+		lookback: effect_lookaxis_nocomeback(-30, 1000, 100, AXIS_Y)
 	};
 	
 }
@@ -45,6 +47,23 @@ var effect_animation2Start = function(speed, nGyros) {
     };
 }
 
+
+var effect_lookaxis_nocomeback = function(degrees, time, steps, axis) {
+    return function () {
+		//pause the game
+        game.pause = true;
+		var initialRot = saveRotation(game.camera, axis); //save initial rotation
+		var increment = degrees / steps / 2;
+		var interval = time / steps;
+		
+        for (var i = 0; i < steps; i++) {
+			setTimeout(rotateAngleInAxisFunction(axis, game.camera, -increment, initialRot), interval*i);
+        }
+        setTimeout(unpauseGameFunction, time);
+    };
+}
+
+
 /**
  *	Look rotating in an axis 
  */
@@ -58,11 +77,12 @@ var effect_lookaxis = function(degrees, time, steps, axis) {
 		
         for (var i = 0; i < steps; i++) {
 			if (i < steps/2) {
-				setTimeout(rotateAngleInAxisFunction(axis, game.camera, -increment * i, initialRot), interval * i);
+				setTimeout(rotateAngleInAxisFunction(axis, game.camera, -increment, initialRot), interval * i);
 			} else {
-				setTimeout(rotateAngleInAxisFunction(axis, game.camera, -increment * (steps - i), initialRot), time / 2 + interval * i);
+				setTimeout(rotateAngleInAxisFunction(axis, game.camera, increment, initialRot), time / 2 + interval * i);
 			}
         }
+		//setTimeout(unpauseGameFunction, time);
         setTimeout(restoreGameFunction(axis, game.camera, initialRot), time);
     };
 }
@@ -80,9 +100,9 @@ function saveRotation(camera, axis) {
 function rotateAngleInAxisFunction(axis, camera, angle, reference) {
 	return function () {
 		switch (axis) {
-			case AXIS_X: camera.rotation.x = reference + angle * 2 * Math.PI / 180; break;
-			case AXIS_Y: camera.rotation.y = reference + angle * 2 * Math.PI / 180; break;
-			case AXIS_Z: camera.rotation.z = reference + angle * 2 * Math.PI / 180; break;
+			case AXIS_X: camera.rotation.x += angle * 2 * Math.PI / 180; break;
+			case AXIS_Y: camera.rotation.y += angle * 2 * Math.PI / 180; break;
+			case AXIS_Z: camera.rotation.z += angle * 2 * Math.PI / 180; break;
 		}
 	}
 }
@@ -98,4 +118,6 @@ function restoreGameFunction(axis, camera, reference) {
 	}
 }
 
-
+var unpauseGameFunction = function() {
+		game.pause = false;
+}
